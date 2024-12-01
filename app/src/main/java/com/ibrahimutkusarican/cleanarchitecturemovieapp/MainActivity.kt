790f1d61d0d480d0e224662c72dbad4ccab5e6a7
usarican <iu.sarican@gmail.com>
2024-12-01 21:42:18 +0300

    Initial Commit

---
+++ app/src/main/java/com/ibrahimutkusarican/cleanarchitecturemovieapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.ibrahimutkusarican.cleanarchitecturemovieapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ibrahimutkusarican.cleanarchitecturemovieapp.ui.theme.CleanArchitectureMovieAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CleanArchitectureMovieAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CleanArchitectureMovieAppTheme {
+        Greeting("Android")
+    }
+}

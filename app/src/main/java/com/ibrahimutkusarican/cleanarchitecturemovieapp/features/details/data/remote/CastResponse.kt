44b2bdf6aaaaa689e622a9aaaa75a53b26767d66
ibrahimutkusarican <iu.sarican@gmail.com>
2025-01-23 14:06:26 +0300

    Response added.

---
+++ app/src/main/java/com/ibrahimutkusarican/cleanarchitecturemovieapp/features/details/data/remote/CastResponse.kt
@@ -0,0 +1,23 @@
+package com.ibrahimutkusarican.cleanarchitecturemovieapp.features.details.data.remote
+
+
+import com.squareup.moshi.Json
+
+data class CastResponse(
+    @Json(name = "cast_id")
+    val castId: Int,
+    @Json(name = "character")
+    val character: String,
+    @Json(name = "gender")
+    val gender: Int,
+    @Json(name = "id")
+    val id: Int,
+    @Json(name = "name")
+    val name: String,
+    @Json(name = "order")
+    val order: Int,
+    @Json(name = "original_name")
+    val originalName: String,
+    @Json(name = "profile_path")
+    val profilePath: String
+)
--- app/src/main/java/com/ibrahimutkusarican/cleanarchitecturemovieapp/features/details/data/remote/MovieDetailService.kt
+++ app/src/main/java/com/ibrahimutkusarican/cleanarchitecturemovieapp/features/details/data/remote/MovieDetailService.kt
@@ -1,9 +1,22 @@
 package com.ibrahimutkusarican.cleanarchitecturemovieapp.features.details.data.remote
 
+import com.ibrahimutkusarican.cleanarchitecturemovieapp.features.home.data.remote.response.MovieResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface MovieDetailService {
     @GET("movie/{movie_id}")
     suspend fun getMovieDetailByMovieId(@Path("movie_id") movieId: Int): MovieDetailResponse
+
+    @GET("movie/{movie_id}/credits")
+    suspend fun getMovieCreditsByMovieId(@Path("movie_id") movieId: Int): MovieDetailCreditResponse
+
+    @GET("movie/{movie_id}/recommendations")
+    suspend fun getMovieRecommendationsByMovieId(@Path("movie_id") movieId: Int): MovieResponse
+
+    @GET("movie/{movie_id}/reviews")
+    suspend fun getMovieReviewsByMovieId(@Path("movie_id") movieId: Int): MovieReviewResponse
+
+    @GET("movie/{movie_id}/videos")
+    suspend fun getMovieVideosByMovieId(@Path("movie_id") movieId: Int): MovieVideoResponse
 }

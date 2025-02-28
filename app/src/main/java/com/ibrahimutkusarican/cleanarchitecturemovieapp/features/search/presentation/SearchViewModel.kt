6244a140b3daf8102d2e79d843a7959227ba8ca3
ibrahimutkusarican <iu.sarican@gmail.com>
2025-02-28 15:21:53 +0300

    MA-28: Last Search Implementation fix: Empty search should not save it.

--- app/src/main/java/com/ibrahimutkusarican/cleanarchitecturemovieapp/features/search/presentation/SearchViewModel.kt
+++ app/src/main/java/com/ibrahimutkusarican/cleanarchitecturemovieapp/features/search/presentation/SearchViewModel.kt
@@ -1,6 +1,5 @@
 package com.ibrahimutkusarican.cleanarchitecturemovieapp.features.search.presentation
 
-import android.util.Log
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import androidx.paging.cachedIn

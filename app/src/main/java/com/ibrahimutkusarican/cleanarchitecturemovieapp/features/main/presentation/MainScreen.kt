9f0e561d5fe4973c601ebcd1d38426e64ef9d6a7
ibrahimutkusarican <iu.sarican@gmail.com>
2025-03-13 17:43:25 +0300

    MA-39: Share Button Implementation fix: Bottom navigation bar inset fix.

--- app/src/main/java/com/ibrahimutkusarican/cleanarchitecturemovieapp/features/main/presentation/MainScreen.kt
+++ app/src/main/java/com/ibrahimutkusarican/cleanarchitecturemovieapp/features/main/presentation/MainScreen.kt
@@ -1,7 +1,11 @@
 package com.ibrahimutkusarican.cleanarchitecturemovieapp.features.main.presentation
 
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -11,6 +15,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
@@ -65,7 +70,7 @@ fun MainScreen(viewModel: MainViewModel) {
     }
     Scaffold(modifier = Modifier.fillMaxSize(), bottomBar = {
         BottomNavigationBar(
-            modifier = Modifier,
+            modifier = Modifier.windowInsetsPadding(WindowInsets.navigationBars),
             navController = navController,
             selectedItemIndex = selectedItemIndex,
             onItemSelected = { index ->
@@ -76,7 +81,9 @@ fun MainScreen(viewModel: MainViewModel) {
         NavHost(
             navController = navController,
             startDestination = NavigationRoutes.BottomNavRoutes.Home,
-            modifier = Modifier.padding(bottom = innerPadding.calculateBottomPadding())
+            modifier = Modifier
+                .windowInsetsPadding(WindowInsets.navigationBars)
+                .padding(vertical = innerPadding.calculateRightPadding(LayoutDirection.Ltr)),
         ) {
             composable<NavigationRoutes.BottomNavRoutes.Home> {
                 val homeViewModel = hiltViewModel<HomeViewModel>()

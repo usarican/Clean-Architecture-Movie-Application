5d12cb7ffed7c506a4dc94cff215f680bb6d7939
ibrahimutkusarican <iu.sarican@gmail.com>
2025-03-21 23:49:18 +0300

    Background should not clickable

--- app/src/main/java/com/ibrahimutkusarican/cleanarchitecturemovieapp/features/details/presentation/DetailScreen.kt
+++ app/src/main/java/com/ibrahimutkusarican/cleanarchitecturemovieapp/features/details/presentation/DetailScreen.kt
@@ -8,6 +8,7 @@ import androidx.annotation.StringRes
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
@@ -49,6 +50,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -578,12 +580,14 @@ private fun MovieDetailImage(
 private fun PlayView(videoKey: String, handleUiAction: (action: DetailUiAction) -> Unit) {
     val context = LocalContext.current
     val lifeCycleOwner = LocalLifecycleOwner.current
+    val interactionSource = remember { MutableInteractionSource() }
 
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.background.copy(alpha = .975F))
-            .windowInsetsPadding(WindowInsets.navigationBars),
+            .windowInsetsPadding(WindowInsets.navigationBars)
+            .clickable(interactionSource = interactionSource, indication = null, onClick = {}),
         contentAlignment = Alignment.Center
     ) {
         Card(

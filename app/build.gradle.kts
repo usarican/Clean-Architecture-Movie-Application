03f181ba844deb4b7e3b49de7cb9268247591ae5
ibrahimutkusarican <iu.sarican@gmail.com>
2025-03-25 11:53:19 +0300

    Removed secret.properties Removed unused constants.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,14 +53,6 @@
                 excludes += "/META-INF/{AL2.0,LGPL2.1}"
             }
         }
-
-        secrets {
-            propertiesFileName = "secrets.properties"
-            defaultPropertiesFileName = "local.defaults.properties"
-
-            ignoreList.add("keyToIgnore")
-            ignoreList.add("sdk.*")
-        }
     }
 
     dependencies {

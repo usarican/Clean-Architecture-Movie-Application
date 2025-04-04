a5b8f2615f55fffc787607948137a4edfdcc181d
İbrahim Can Erdoğan <52867508+icanerdogan@users.noreply.github.com>
2025-04-04 16:58:39 +0300

    Refactor: Introduce core module and restructure Gradle configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,124 +1,14 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.compose.compiler)
-    alias(libs.plugins.kotlin.ksp)
-    alias(libs.plugins.hilt.android)
-    alias(libs.plugins.android.room)
-    alias(libs.plugins.google.mapsplatform)
     kotlin("plugin.serialization") version "2.1.0"
 }
 
 android {
     namespace = "com.ibrahimutkusarican.cleanarchitecturemovieapp"
-    compileSdk = 35
-
-    defaultConfig {
-        applicationId = "com.ibrahimutkusarican.cleanarchitecturemovieapp"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
-    buildFeatures {
-        compose = true
-        buildConfig = true
-    }
-    room {
-        schemaDirectory("$projectDir/schemas")
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
 }
 
+apply(from = "$rootDir/scripts/common.gradle")
+
 dependencies {
-    // Core AndroidX Libraries
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.lifecycle.livedata)
-    implementation(libs.androidx.lifecycle.viewmodel)
-    implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.datastore.preferences)
-    implementation(libs.androidx.palette.ktx)
-
-    // Jetpack Compose
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.ui.text.google.fonts)
-    implementation(libs.androidx.animation.graphics.android)
-    implementation(libs.androidx.material3)
-    implementation(libs.androidx.constraintlayout.compose)
-
-    // Navigation
-    implementation(libs.androidx.navigation)
-    implementation(libs.androidx.hilt.navigation.compose)
-
-    // Dependency Injection (Hilt)
-    implementation(libs.hilt.android)
-    ksp(libs.hilt.compiler)
-
-    // Networking & Serialization
-    implementation(libs.kotlinx.serialization.json)
-    implementation(libs.retrofit)
-    implementation(libs.moshi.converter)
-    implementation(libs.moshi)
-    implementation(libs.okhttp)
-    implementation(libs.logging.interceptor)
-    ksp(libs.moshi.codegen)
-
-    // Room Database
-    implementation(libs.room.runtime)
-    implementation(libs.room.ktx)
-    ksp(libs.room.compiler)
-
-    // Paging
-    implementation(libs.androidx.paging.runtime)
-    implementation(libs.androidx.paging.compose)
-    implementation(libs.room.paging)
-
-    // Image Loading & Animations
-    implementation(libs.coil.compose)
-    implementation(libs.coil.network.okhttp)
-    implementation(libs.dotlottie.android)
-
-    // YouTube Player
-    implementation(libs.youtube.player)
-
-    // Testing Dependencies
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-
-    // Debugging Tools
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(project(":core"))
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     alias(libs.plugins.hilt.android) apply false
     alias(libs.plugins.kotlin.ksp) apply false
     alias(libs.plugins.android.room) apply false
+    alias(libs.plugins.android.library) apply false
 }
 buildscript {
     dependencies {

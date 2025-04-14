fd0fb3d5557d974fc0eb7e7cf6aeef2549ff855e
ibrahimutkusarican <iu.sarican@gmail.com>
2025-04-14 08:16:36 +0300

    Cama-2: Multi-Moduler Architecture feat: AppConfig object added.

---
+++ build-logic/convention/src/main/java/com/iusarican/config/AppConfig.kt
@@ -0,0 +1,10 @@
+package com.iusarican.config
+
+import org.gradle.api.JavaVersion
+
+object AppConfig {
+    const val TARGET_SDK = 35
+    const val COMPILE_SDK = 35
+    const val MIN_SDK = 24
+    val JAVA_VERSION = JavaVersion.VERSION_17
+}

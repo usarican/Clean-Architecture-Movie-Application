0c7acb63742750e18007ad8c4c56e4b61bc1b3b8
ibrahimutkusarican <iu.sarican@gmail.com>
2025-04-17 21:53:31 +0300

    Cama-2: Multi-Moduler Architecture refactor: Exception extension write and make more reusable to baseRepository apiCall function

--- app/src/main/java/com/ibrahimutkusarican/cleanarchitecturemovieapp/core/base/BaseRepository.kt
+++ app/src/main/java/com/ibrahimutkusarican/cleanarchitecturemovieapp/core/base/BaseRepository.kt
@@ -1,47 +1,18 @@
 package com.ibrahimutkusarican.cleanarchitecturemovieapp.core.base
 
-import android.database.sqlite.SQLiteException
-import com.ibrahimutkusarican.cleanarchitecturemovieapp.core.exception.MovieException
 import com.ibrahimutkusarican.cleanarchitecturemovieapp.core.action.ApiState
+import com.ibrahimutkusarican.cleanarchitecturemovieapp.core.exception.toMovieException
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.TimeoutCancellationException
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
-import retrofit2.HttpException
-import java.io.IOException
 
 abstract class BaseRepository {
     fun <T : Any?> apiCall(call: suspend () -> T): Flow<ApiState<T>> = flow {
         emit(ApiState.Success(data = call.invoke()) as ApiState<T>)
     }.catch { exp ->
         exp.printStackTrace()
-        val error = when (exp) {
-            is IOException -> MovieException.NoInternetException("No Internet Connection")
-            is HttpException -> {
-                when (exp.code()) {
-                    UNAUTHORIZED_ERROR_CODE -> MovieException.UnauthorizedException("Unauthorized Access")
-                    NOT_FOUND_ERROR_CODE -> MovieException.NotFoundException("Resource Not Found")
-                    INTERNAL_SERVER_ERROR_CODE -> MovieException.InternalServerErrorException("Internal Server Error")
-                    else -> MovieException.GeneralHttpException(
-                        code = exp.code(), message = "HTTP Error: ${exp.code()} ${exp.message}"
-                    )
-
-                }
-            }
-            is coil3.network.HttpException -> MovieException.CoilHttpException(exp.message)
-            is SQLiteException -> MovieException.GeneralException("Database Error: ${exp.message}")
-            is TimeoutCancellationException -> MovieException.GeneralException("Request Timeout")
-            else -> MovieException.GeneralException("Unknown Error: ${exp.localizedMessage}")
-        }
-        emit(ApiState.Error(error))
+        emit(ApiState.Error(exp.toMovieException()))
     }.flowOn(Dispatchers.IO)
-
-
-    companion object {
-        private const val UNAUTHORIZED_ERROR_CODE = 401
-        private const val NOT_FOUND_ERROR_CODE = 404
-        private const val INTERNAL_SERVER_ERROR_CODE = 500
-    }
 }
--- app/src/main/java/com/ibrahimutkusarican/cleanarchitecturemovieapp/core/exception/MovieException.kt
+++ app/src/main/java/com/ibrahimutkusarican/cleanarchitecturemovieapp/core/exception/MovieException.kt
@@ -1,6 +1,11 @@
 package com.ibrahimutkusarican.cleanarchitecturemovieapp.core.exception
 
-sealed class MovieException(message : String?) : Throwable(message){
+import android.database.sqlite.SQLiteException
+import kotlinx.coroutines.TimeoutCancellationException
+import retrofit2.HttpException
+import java.io.IOException
+
+sealed class MovieException(message: String?) : Throwable(message) {
     class NoInternetException(message: String?) : MovieException(message)
     class CoilHttpException(message: String?) : MovieException(message)
     class UnauthorizedException(message: String?) : MovieException(message)
@@ -9,3 +14,31 @@ sealed class MovieException(message : String?) : Throwable(message){
     class GeneralHttpException(val code: Int, message: String?) : MovieException(message)
     class GeneralException(message: String?) : MovieException(message)
 }
+
+fun Throwable.toMovieException(): MovieException {
+    return when (this) {
+        is IOException -> MovieException.NoInternetException("No Internet Connection")
+        is HttpException -> {
+            when (code()) {
+                UNAUTHORIZED_ERROR_CODE -> MovieException.UnauthorizedException("Unauthorized Access")
+                NOT_FOUND_ERROR_CODE -> MovieException.NotFoundException("Resource Not Found")
+                INTERNAL_SERVER_ERROR_CODE -> MovieException.InternalServerErrorException("Internal Server Error")
+                else -> MovieException.GeneralHttpException(
+                    code = code(),
+                    message = "HTTP Error: ${code()} $message"
+                )
+
+            }
+        }
+
+        is coil3.network.HttpException -> MovieException.CoilHttpException(message)
+        is SQLiteException -> MovieException.GeneralException("Database Error: $message")
+        is TimeoutCancellationException -> MovieException.GeneralException("Request Timeout")
+        else -> MovieException.GeneralException("Unknown Error: $localizedMessage")
+    }
+}
+
+
+private const val UNAUTHORIZED_ERROR_CODE = 401
+private const val NOT_FOUND_ERROR_CODE = 404
+private const val INTERNAL_SERVER_ERROR_CODE = 500
